3365d4174d67e9e283303592545fae719575263f
ravikumar-darsi <ravikumardarsi90@gmail.com>
2024-06-01 11:27:29 +0530

    changed ip to --> domain names

--- mysql.sh
+++ mysql.sh
@@ -5,7 +5,7 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-MONGDB_HOST=172.31.23.198
+MONGDB_HOST=mongodb.daws76s.online
 
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
--- user.sh
+++ user.sh
@@ -8,7 +8,7 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-MONGDB_HOST=172.31.23.198
+MONGDB_HOST=mongodb.daws67s.online
 
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
