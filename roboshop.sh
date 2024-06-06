d4bec86cc74673deb99190dd7b15ac74303aa96b
ravikumar-darsi <ravikumardarsi90@gmail.com>
2024-06-06 12:43:44 +0530

    automating instance creation nd displaying pvt IP

--- roboshop.sh
+++ roboshop.sh
@@ -8,7 +8,6 @@ DOMAIN_NAME="daws67s.online"
 
 for i in "${INSTANCES[@]}"
 do
-    echo "instance is: $i"
     if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
     then
         #INSTANCE_TYPE="t3.small"
@@ -16,5 +15,6 @@ do
     else
         INSTANCE_TYPE="t2.micro"
     fi
-    aws ec2 run-instances --image-id ami-0b4f379183e5706b9  --instance-type $INSTANCE_TYPE --security-group-ids sg-093a057bce942d496 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
+    IP_ADDRESS=$(aws ec2 run-instances --image-id ami-0b4f379183e5706b9  --instance-type $INSTANCE_TYPE --security-group-ids sg-093a057bce942d496 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"  --query 'Instances[0].PrivateIpAddress' --output text)
+    echo "$i: $IP_ADDRESS"
 done
