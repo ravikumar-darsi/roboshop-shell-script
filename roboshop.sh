acdf11b0f0f76ebd1d229f31152f48d2737b03bf
ravikumar-darsi <ravikumardarsi90@gmail.com>
2024-09-10 11:47:52 +0530

    sg also changed

--- roboshop.sh
+++ roboshop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 AMI=ami-0b4f379183e5706b9 #this keeps on changing
-SG_ID=sg-093a057bce942d496 #replace with your SG ID
+SG_ID=sg-07defaa128992b3ec #replace with your SG ID
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
 ZONE_ID=Z0010052P5AJGIQ7R5VQ # replaced my zone ID
 DOMAIN_NAME="daws67s.online"
@@ -16,7 +16,7 @@ do
     else
         INSTANCE_TYPE="t2.micro"
     fi
-    IP_ADDRESS=$(aws ec2 run-instances --image-id ami-0b4f379183e5706b9  --instance-type $INSTANCE_TYPE --security-group-ids sg-093a057bce942d496 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"  --query 'Instances[0].PrivateIpAddress' --output text)
+    IP_ADDRESS=$(aws ec2 run-instances --image-id ami-0b4f379183e5706b9  --instance-type $INSTANCE_TYPE --security-group-ids sg-07defaa128992b3ec --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"  --query 'Instances[0].PrivateIpAddress' --output text)
     echo "$i: $IP_ADDRESS"
 
      #create R53 record, make sure you delete existing record
